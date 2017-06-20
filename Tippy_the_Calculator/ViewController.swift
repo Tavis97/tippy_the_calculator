4811b505804379cdf43c04d1e1ccb7ccb47a8704
Tavis Thompson <tavis97@fb.com>
2017-06-20 13:27:47 -0700

    Initial Commit

---
+++ Tippy_the_Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy_the_Calculator
+//
+//  Created by Tavis Thompson on 6/20/17.
+//  Copyright © 2017 Tavis Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tippy_the_CalculatorTests/Tippy_the_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tippy_the_CalculatorTests.swift
+//  Tippy_the_CalculatorTests
+//
+//  Created by Tavis Thompson on 6/20/17.
+//  Copyright © 2017 Tavis Thompson. All rights reserved.
+//
+
+import XCTest
+@testable import Tippy_the_Calculator
+
+class Tippy_the_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
